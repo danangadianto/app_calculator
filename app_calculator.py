b83043ef78a96bb19bdb82785cb8c3b255c80db3
Danang Adianto <danangadianto1453@gmail.com>
2025-12-29 19:25:05 +0700

    full featured

--- app_calculator.py
+++ app_calculator.py
@@ -4,6 +4,7 @@ def addition():
         num2 = int(input("Enter first number: "))
         result = num1 + num2
         print(f"Result: {result}")
+        input("Press Enter to continue...")
     except:
         print("Invalid input. Please enter a valid number.")
 
@@ -13,6 +14,7 @@ def subtraction():
         num2 = int(input("Enter first number: "))
         result = num1 - num2
         print(f"Result: {result}")
+        input("Press Enter to continue...")
     except:
         print("Invalid input. Please enter a valid number.")
 
@@ -22,6 +24,7 @@ def multiplication():
         num2 = int(input("Enter first number: "))
         result = num1 * num2
         print(f"Result: {result}")
+        input("Press Enter to continue...")
     except:
         print("Invalid input. Please enter a valid number.")
 
@@ -31,5 +34,35 @@ def division():
         num2 = int(input("Enter first number: "))
         result = num1 / num2
         print(f"Result: {result}")
+        input("Press Enter to continue...")
     except:
         print("Invalid input. Please enter a valid number.")
+
+def app_menu():
+    print("===SIMPLE CALCULATOR===")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    while True:
+        try:
+            user_input = int(input("Enter your choice (1-5): "))
+            if user_input == 1:
+                addition()
+            elif user_input == 2:
+                subtraction()
+            elif user_input == 3:
+                multiplication()
+            elif user_input == 4:
+                division()
+            elif user_input == 5:
+                print("Thank you for using the calculator.")
+                break
+            else:
+                print("Invalid choice. Please enter a number between 1 and 5.")
+        except:
+            print("Invalid input. Please enter a valid number.")
+
+app_menu()
